33b57da95e4c889a723bcb596506476cbbec9d6b
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-19 05:27:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 # numbers = [1,2,3]
 
 def square_array(array)
-  array.each do |squared|
-    new_array = []
-    new_array << array * 2
+  new_array = []
+  array.each { |squared| new_array << array * 2}
   end
 end
 
